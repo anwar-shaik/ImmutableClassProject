e9a217fab2928cfe0a1b2098c0a9ccc355e61715
anwar-shaik <ANWAR.ATG@GMAIL.COM>
2020-09-06 15:26:53 -0400

    Custome Immutable class

---
+++ src/TestMain.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ImmutableClass im = ImmutableClass.createNewInstance(100, "test", new Date());
+		System.out.println(im);
+		tryModification(im.getImmutableField1(), im.getImmutableField2(), im.getMutableField());
+		System.out.println(im);
+	}
+
+	private static void tryModification(Integer immutableField1, String immutableField2, Date mutableField) {
+		immutableField1 = 10000;
+		immutableField2 = "test changed";
+		mutableField.setDate(10);
+	}
+
+
+}
